38ba4be8842bb5eaa88e0856f049f7df4214b3e7
sven <osirotonny@gmail.com>
2026-08-13 17:16:10 +0300

    C function to swap the value of integers a and b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * swap_int - Swaps the values of two integers.
+  * @a: A pointer to the first integer.
+  * @b: A pointer to the second integer.
+  * @temp: variable declared to temporarily hold the value of a before 
+  * switching.
+  *
+  * Return: Nothing.
+  */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
