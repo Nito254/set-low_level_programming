129c6057e96ad9d34de35963bff551f644b6330d
sven <osirotonny@gmail.com>
2026-08-13 17:57:06 +0300

    C function that reverses a string using a while loop and a temp variable

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * rev_string - Reverses a string.
+  * @s: The string to be reversed.
+  *
+  * Return: Nothing.
+  */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+	char temp;
+
+	/* Find the length of the string */
+	while (s[len] != '\0')
+	{
+		len++
+	}
+
+	len--; /* Adjust to the index of the last printable character */
+
+	/* Swap characters from the outside in */
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
+		len--;
+	}
+}
