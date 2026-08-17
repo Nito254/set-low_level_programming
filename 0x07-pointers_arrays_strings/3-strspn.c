f24f56f224f7d57ddabb4b8acf9d501482056dcb
sven <osirotonny@gmail.com>
2026-08-17 15:36:00 +0300

    C function to get the length of a prefix substring and return the number of bytes in the initial egment of tstring to be searched

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * _strspn - Gets the length of a prefix substring.
+  * @s: The string to be searched.
+  * @accept: The string containing accepted bytes.
+  *
+  * Return: The number of bytes in the initial segment of @s which consist
+  	    only of bytes fom @accept.
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j= 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			return (count);
+	}
+
+	return (count);
+}
