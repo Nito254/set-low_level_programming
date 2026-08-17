719040d38a1ed121975a53ec0e23c37f49feee70
sven <osirotonny@gmail.com>
2026-08-17 16:14:27 +0300

    Fixed logical error

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -20,7 +20,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			for (j = 0; needle[j] != needle[j])
+			if (haystack[i + j] != needle[j])
 				break;
 		}
 
